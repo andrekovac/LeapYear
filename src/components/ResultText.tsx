0d92ee470b2eec232d8303c862a141b3fd8b73d7
André Kovac <me@andrekovac.com>
2020-09-16 13:11:19 +0200

    Add fully animated app with functional TextField

---
+++ src/components/ResultText.tsx
@@ -0,0 +1,51 @@
+import React, {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useRef,
+} from "react";
+import { Animated } from "react-native";
+
+import Text from "./Text";
+
+const ResultText: FunctionComponent<{ year?: string }> = ({ year }) => {
+  const springAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    letterJump();
+    return () => {
+      springAnim.setValue(0);
+    };
+  }, [year]);
+
+  const letterJump = useCallback(() => {
+    Animated.spring(springAnim, {
+      toValue: 1,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
+  return (
+    <>
+      <Text>The year</Text>
+      <Text
+        style={{
+          transform: [
+            {
+              scale: springAnim.interpolate({
+                inputRange: [0, 0.5, 1],
+                outputRange: [0.9, 1.1, 1],
+              }),
+            },
+          ],
+        }}
+        size={90}
+      >
+        {year || "0"}
+      </Text>
+      <Text>is a leap year 🎉</Text>
+    </>
+  );
+};
+
+export default ResultText;
