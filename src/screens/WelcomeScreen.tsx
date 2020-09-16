b68223b394a65e48bbdf0e9718ab8929295a2c07
André Kovac <me@andrekovac.com>
2020-09-16 12:49:56 +0200

    Add spring animation to Welcome screen

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,19 +1,78 @@
-import React, { FunctionComponent } from "react";
+import React, { useEffect, useRef, FunctionComponent } from "react";
+import { Animated, Easing } from "react-native";
+import styled from "styled-components/native";
 
 import Text from "../components/Text";
-import StartButton from "../components/StartButton";
 
 type WelcomeScreenProps = {
   onPress: () => void;
+  hasPressedButton: boolean;
 };
 
-const WelcomeScreen: FunctionComponent<WelcomeScreenProps> = ({ onPress }) => {
-  return (
-    <>
-      <Text size={80}>{"Leap Year"}</Text>
-      <StartButton text={"Start"} onPress={onPress} />
-    </>
-  );
+const WelcomeScreen: FunctionComponent<WelcomeScreenProps> = ({
+  onPress,
+  hasPressedButton,
+}) => {
+  const introAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    bounceIn();
+    return () => {
+      introAnim.setValue(0);
+    };
+  }, [hasPressedButton]);
+
+  const bounceIn = () => {
+    Animated.timing(introAnim, {
+      toValue: 1,
+      duration: 1000,
+      easing: Easing.bounce,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  return <Wrapper>
+  <Text
+    style={{
+      transform: [
+        {
+          scale: introAnim.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1, 2],
+          }),
+        },
+      ],
+    }}
+  >
+    LeapYear
+  </Text>
+  <StyledButton onPress={onPress}>
+    <TextWrapper>
+      <Text>Start</Text>
+    </TextWrapper>
+  </StyledButton>
+</Wrapper>;
 };
+
+const Wrapper = styled.View`
+  flex: 1;
+  width: 100%;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 10px;
+  background-color: #ff0e64;
+`;
+
+const StyledButton = styled.TouchableOpacity`
+  width: 100%;
+`;
+
+const TextWrapper = styled.View`
+  padding: 10px;
+  background-color: white;
+  border-radius: 35px;
+
+  align-items: center;
+`;
 
 export default WelcomeScreen;
