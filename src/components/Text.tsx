0c4bfdadffb909b5aacfde3d1922ec2b015a185c
André Kovac <me@andrekovac.com>
2020-09-20 18:52:50 +0200

    Update Text component and snapshot tests

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,10 +1,11 @@
 import { Animated } from "react-native";
 import styled from "styled-components/native";
 
-const Text = styled(Animated.Text)<{ size?: number }>`
+const Text = styled(Animated.Text)<{ size?: number, fontWeight?: number }>`
   font-size: ${(props) => (props.size ? `${props.size}px` : "30px")};
   font-weight: 100;
   text-align: center;
+  color: black;
 `;
 
 export default Text;
